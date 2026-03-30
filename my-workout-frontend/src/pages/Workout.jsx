21265179bb4f105c1bdde87c34153aa91912fd58
elias <eliasnerylima018@gmail.com>
2026-03-30 12:22:28 -0300

    feat: adiciona novo exercício ao treino

--- my-workout-frontend/src/pages/Workout.jsx
+++ my-workout-frontend/src/pages/Workout.jsx
@@ -2,47 +2,69 @@ import { useParams, useLocation, useNavigate } from "react-router-dom"
 import { useEffect } from "react"
 import { useState } from "react"
 import { getExercises } from "../services/exercises"
+import { addExercicio } from "../services/exercises"
 
 const Workout = () => {
 
     const navigate = useNavigate()
     const { id } = useParams()
-    const location = useLocation()  
+    const location = useLocation()
 
     const [exercicios, setExercicios] = useState([])
+    const [exercicio, setExercicio] = useState("")
 
     const nomeTreino = location.state?.nomeTreino || `Treino ${id}`
-    
-    useEffect(() => {
 
+    const showEx = async () => {
         const token = localStorage.getItem("token")
-        const showEx = async () => {
-
-            try {
-                const data = await getExercises(token, id)
-                setExercicios(data.ex || [])
-            } catch (error) {
-                console.error("Erro ao buscar exercícios:", error)
-            }
+        try {
+            const data = await getExercises(token, id)
+            setExercicios(data.ex || [])
+        } catch (error) {
+            console.error("Erro ao buscar exercícios:", error)
         }
+    }
 
+    useEffect(() => {
         showEx()
     }, [id])
 
+
+    const handleExercicio = async (e) => {
+        e.preventDefault()
+
+        try {
+            const token = localStorage.getItem("token")
+            const data = await addExercicio(token, id, exercicio)
+
+            console.log("Exercício cadastrado!", data)
+            setExercicio("")
+            showEx()
+        } catch (error) {
+            console.error("Erro ao criar exercício!", error) 
+            alert("Falha: ", error.message)
+        }
+
+    }
 
     return (<>
         <div className="top">
             <h1>{nomeTreino}</h1>
             <button onClick={() => navigate("/Dashboard")}>Voltar</button>
-            <button className="btn-novo-ex">Adicionar Exercício</button>
+
+            <form onSubmit={handleExercicio}>
+                <input type="text" value={exercicio} onChange={(e) => setExercicio(e.target.value)} />
+                <button type="submit" className="btn-novo-ex">Adicionar Exercício</button>
+            </form>
+
         </div>
         <h2>Exercícios do treino</h2>
         <div className="exercicios">
             {exercicios?.map((item) => (
-                    <div key={item.id} className="ex-item">
-                        <h3>{item.nome}</h3>
-                    </div>
-                ))}
+                <div key={item.id} className="ex-item">
+                    <h3>{item.nome}</h3>
+                </div>
+            ))}
         </div>
     </>)
 }
--- my-workout-frontend/src/services/exercises.js
+++ my-workout-frontend/src/services/exercises.js
@@ -1,3 +1,4 @@
+import { data } from "react-router-dom"
 import BASE_URL from "./api"
 
 export async function getExercises(token, id_workout) {
@@ -17,4 +18,24 @@ export async function getExercises(token, id_workout) {
     const data = await res.json()
     return data
 
+}
+
+export async function addExercicio(token, id_workout, nome) {
+
+    const res = await fetch(`${BASE_URL}/workout/exercise/${id_workout}`, {
+        method: 'POST',
+        headers: {
+            "Authorization" : `Bearer ${token}`,
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({nome})
+    })
+
+    if(!res.ok){
+        const dataError = await res.json()
+        throw new Error("Erro ao adicionar novo exercício!", dataError.error)
+    }
+
+    const data = await res.json()
+    return data
 }
