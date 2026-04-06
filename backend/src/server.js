13ddab09785e0638ba95cd507267edabce1c1705
elias <eliasnerylima018@gmail.com>
2026-04-06 15:13:54 -0300

    fix: config de backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,11 @@
 import app from './app.js'
+import dotenv from 'dotenv'
 
-app.listen(3050)
+dotenv.config()
+
+const PORT = process.env.PORT || 3050; 
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
